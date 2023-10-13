c70e4dea1c94a6a1320e3914a25f3fa8720798e4
thomas <thomas.moissl@mni.thm.de>
2023-10-13 15:53:02 +0200

    Metal Detector (Advanced Items)

--- src/main/java/net/motaso/wobble/item/ModItems.java
+++ src/main/java/net/motaso/wobble/item/ModItems.java
@@ -6,6 +6,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.motaso.wobble.Wobble;
+import net.motaso.wobble.item.custom.MetalDetectorItem;
 
 public class ModItems {
     // List of Items
@@ -15,9 +16,11 @@ public class ModItems {
     // Actual Items
     public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
             () -> new Item(new Item.Properties()));
-
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
             () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+            () -> new MetalDetectorItem(new Item.Properties().durability(100)));
 
 
     // Has to get registered in Wobble File as well
