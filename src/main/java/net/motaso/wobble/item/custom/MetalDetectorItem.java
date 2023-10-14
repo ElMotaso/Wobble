801cc3e872849ad962a14407b79ab9eea26b08fd
thomas <thomas.moissl@mni.thm.de>
2023-10-14 16:49:59 +0200

    Custom Tags

--- src/main/java/net/motaso/wobble/item/custom/MetalDetectorItem.java
+++ src/main/java/net/motaso/wobble/item/custom/MetalDetectorItem.java
@@ -6,10 +6,17 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.motaso.wobble.util.ModTags;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Properties pProperties) {
@@ -44,14 +51,21 @@ public class MetalDetectorItem extends Item {
         return InteractionResult.SUCCESS;
     }
 
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pToolTipComponents, TooltipFlag pIsAdvanced) {
+        pToolTipComponents.add(Component.translatable("tooltip.wobble.metal_detector.tooltip"));
+        super.appendHoverText(pStack, pLevel, pToolTipComponents, pIsAdvanced);
+    }
+
     private void outputValuableCoordinates(BlockPos blockPos, Player player, Block block) {
         player.sendSystemMessage(Component.literal("Found " + I18n.get(block.getDescriptionId()) + " at " +
                 "(" + blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ() + ")"));
     }
     private boolean isValuableBlock(BlockState state) {
-        return state.is(Blocks.IRON_ORE)
-                || state.is(Blocks.DEEPSLATE_IRON_ORE)
-                || state.is(Blocks.DIAMOND_ORE)
-                || state.is(Blocks.DEEPSLATE_DIAMOND_ORE);
+        // Without custom tag
+        // return state.is(Blocks.IRON_ORE) || state.is(Blocks.DEEPSLATE_IRON_ORE) || state.is(Blocks.DIAMOND_ORE) || state.is(Blocks.DEEPSLATE_DIAMOND_ORE);
+
+        // with custom tag
+        return state.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
     }
 }
