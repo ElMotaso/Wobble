4bc3b212cc535d03bad8182c0a05a7cb883f3b63
thomas <thomas.moissl@mni.thm.de>
2023-10-14 17:56:46 +0200

    Data Generation

---
+++ src/main/java/net/motaso/wobble/datagen/ModItemModelProvider.java
@@ -0,0 +1,35 @@
+package net.motaso.wobble.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+import net.motaso.wobble.Wobble;
+import net.motaso.wobble.item.ModItems;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, Wobble.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        // basicItem methods also works
+
+        simpleItem(ModItems.SAPPHIRE);
+        simpleItem(ModItems.RAW_SAPPHIRE);
+
+        simpleItem(ModItems.METAL_DETECTOR);
+        simpleItem(ModItems.PINE_CONE);
+        simpleItem(ModItems.STRAWBERRY);
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(Wobble.MODID, "item/" + item.getId().getPath()));
+    }
+}
